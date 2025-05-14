7055128f03805d56ac480bfe8f89fc6f6dd26362
osneida <osneidabordones@gmail.com>
2025-05-14 15:35:42 -0400

    iniciando el diccionario de traducciones

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Las credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo en :seconds segundos.',
+    'password_reset' => 'Restablecimiento de contraseña',
+    'password_reset_link' => 'Enviar enlace de restablecimiento de contraseña',
+    'email' => 'Correo electrónico',
+    'password_confirmation' => 'Confirmar contraseña',
+    'remember_me' => 'Recuérdame',
+    'login' => 'Iniciar sesión',
+    'register' => 'Registrar',
+    'name' => 'Nombre',
+    'first_name' => 'Nombre',
+    'last_name' => 'Apellido',
+    'Password' => 'Contraseña',
+    'confirm_password' => 'Confirmar contraseña',
+];
---
+++ resources/lang/es/pagination.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+    'first' => '&laquo; Primero',
+    'last' => 'Último &raquo;',
+    'page' => 'Página :page',
+    'of' => 'de :total',
+    'showing' => 'Mostrando :first a :last de :total resultados',
+    'showing_from' => 'Mostrando desde :first',
+    'showing_to' => 'hasta :last',
+    'showing_of' => 'de :total resultados',
+    'showing_all' => 'Mostrando todos los resultados',
+    'showing_none' => 'No se encontraron resultados',
+    'showing_page' => 'Mostrando página :page de :total',
+    'showing_page_of' => 'Mostrando página :page de :total',
+    'showing_page_of_total' => 'Mostrando página :page de :total',
+    'showing_page_of_total_results' => 'Mostrando página :page de :total resultados',
+    'showing_page_of_total_results_with_count' => 'Mostrando página :page de :total resultados (contando desde :count)',
+    'showing_page_of_total_results_with_count_and_offset' => 'Mostrando página :page de :total resultados (contando desde :count, con un desplazamiento de :offset)',
+    'showing_page_of_total_results_with_count_and_offset_and_limit' => 'Mostrando página :page de :total resultados (contando desde :count, con un desplazamiento de :offset y un límite de :limit)',
+];
